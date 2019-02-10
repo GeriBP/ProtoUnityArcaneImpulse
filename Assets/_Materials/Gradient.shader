008e142d85fef4f2e0b9fe2f1e1bd41e92d6f36e
GeriBP <gbelenguer@gmail.com>
2019-02-10 20:50:54 +0100

    Prepared level template. Mana system implemented

---
+++ Assets/_Materials/Gradient.shader
@@ -0,0 +1,45 @@
+﻿Shader "Custom/Gradient" {
+Properties {
+	_MainTex ("Texture", 2D) = "white" {}
+    _Color ("Bottom Color", Color) = (1,1,1,1)
+    _Color2 ("Top Color", Color) = (1,1,1,1)
+}
+ 
+SubShader {
+    Tags {"Queue"="Background"  "IgnoreProjector"="True"}
+ 
+    Pass {
+        CGPROGRAM
+        #pragma vertex vert  
+        #pragma fragment frag
+        #include "UnityCG.cginc"
+		
+		 struct appdata
+		{
+			float4 vertex : POSITION;
+			float2 uv : TEXCOORD0;
+		};
+
+        struct v2f {
+            float4 pos : SV_POSITION;
+            fixed4 col : COLOR;
+        };
+
+		fixed4 _Color;
+        fixed4 _Color2;
+
+        v2f vert (appdata_full v)
+        {
+            v2f o;
+            o.pos = UnityObjectToClipPos (v.vertex);
+            o.col = lerp(_Color,_Color2, v.texcoord.y);
+            return o;
+        }
+       
+        float4 frag (v2f i) : COLOR {
+            return i.col;
+        }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/_Scripts/Impulse1.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Impulse1 : MonoBehaviour {
+	public float pushFObjects, pushFPlayer, pullFPlayer, pullFObjects, maxDist, minDistPull, maxObjSpeed;
+	public LayerMask mask;
+	public GameObject indicator;
+	public Transform objectPos;
+	public PhysicMaterial playerPhysMat;
+	public static Impulse1 instance;
+    [Header("Mana Bar Stuff")]
+    public float maxMana;
+    public float pushCost, pullPlayerCost, pullObjCostIni, pullObjCostHold, manaRecoveryTime, manaRecoveryAmount;
+
+    private float currMana;
+    private Image manaBar;
+    private Rigidbody rb, targetRb;
+	private RaycastHit hit;
+	private Transform camTransform;
+	private PulledObj objS;
+	private Move moveS;
+    private bool drainMana = false;
+    private float timeToRecharge = 0f;
+
+	private void Awake()
+	{
+		instance = this;
+        manaBar = GameObject.Find("ManaBar").GetComponent<Image>();
+        manaBar.fillAmount = 1.0f;
+    }
+
+	void Start () {
+        currMana = maxMana;
+        rb = GetComponent<Rigidbody>();
+		camTransform = Camera.main.transform;
+		moveS = FindObjectOfType<Move>();
+	}
+	
+	void Update () {
+        Physics.Raycast(camTransform.position, camTransform.forward, out hit, mask);
+		float dist = Vector3.Distance(hit.point, transform.position);
+
+		if (hit.transform != null && dist < maxDist && objS == null)
+		{
+            indicator.SetActive(true);
+			indicator.transform.position = hit.point;
+			targetRb = hit.transform.GetComponent<Rigidbody>();
+			HandlePowerInput(dist);
+		}
+		else if (objS != null)
+		{
+			indicator.SetActive(false);
+			indicator.transform.position = objS.transform.position;
+			HandlePowerInput(dist);
+		}
+		else
+		{
+			indicator.SetActive(false);
+		}
+
+        if (drainMana)
+        {
+            currMana -= pullObjCostHold * Time.deltaTime;
+            timeToRecharge = Time.timeSinceLevelLoad + manaRecoveryTime;
+        }
+        else if (timeToRecharge < Time.timeSinceLevelLoad)
+        {
+            currMana += manaRecoveryAmount * Time.deltaTime;
+        }
+        currMana = Mathf.Clamp(currMana, 0f, maxMana);
+        manaBar.fillAmount = currMana/maxMana;
+    }
+
+	private void HandlePowerInput(float dist)
+	{
+		if (Input.GetButtonDown("Push") && currMana >= pushCost)
+		{
+            currMana -= pushCost;
+            timeToRecharge = Time.timeSinceLevelLoad + manaRecoveryTime;
+            if (objS != null)
+			{
+				objS.CancelPull();
+				targetRb.AddForce(camTransform.forward * pushFObjects, ForceMode.Impulse);
+			}
+			else if (targetRb == null)
+			{
+				NoFriction();
+				rb.AddForce(-camTransform.forward * pushFPlayer, ForceMode.Impulse);
+			}
+			else
+			{
+				targetRb.AddForce(camTransform.forward * pushFObjects, ForceMode.Impulse);
+			}
+		}
+		else if (Input.GetButtonDown("Pull") && currMana >= pullPlayerCost)
+		{
+            currMana -= pullPlayerCost;
+            timeToRecharge = Time.timeSinceLevelLoad + manaRecoveryTime;
+            objS = null;
+			if (targetRb == null)
+			{
+				NoFriction();
+				rb.AddForce(camTransform.forward * pullFPlayer, ForceMode.Impulse);
+			}
+		}
+		else if (Input.GetButton("Pull") && currMana >= pullObjCostIni)
+		{
+			if (targetRb != null && objS == null)
+			{
+                if(!drainMana) currMana -= pullObjCostIni;
+                timeToRecharge = Time.timeSinceLevelLoad + manaRecoveryTime;
+                drainMana = true;
+                if (Vector3.Distance(targetRb.transform.position, objectPos.position) > minDistPull)
+				{
+					targetRb.AddForce(-camTransform.forward * pullFObjects, ForceMode.Force);
+					targetRb.velocity = Vector3.ClampMagnitude(targetRb.velocity, maxObjSpeed);
+				}
+				else
+				{
+					targetRb.velocity = Vector3.zero;
+					objS = targetRb.GetComponent<PulledObj>();
+					objS.GetPulled();
+				}
+			}
+		}
+
+		if (Input.GetButtonUp("Pull"))
+		{
+			if(objS != null) objS.CancelPull();
+			objS = null;
+            drainMana = false;
+        }
+	}
+
+	private void NoFriction()
+	{
+		StopAllCoroutines();
+		StartCoroutine(CantWalkCR());
+	}
+
+	IEnumerator CantWalkCR()
+	{
+		moveS.canWalk = false;
+		yield return new WaitForSeconds(1f);
+		moveS.canWalk = true;
+	}
+}
+ 
